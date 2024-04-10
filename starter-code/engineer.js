27b6316e8a6790d2de55e9d43a883a19124c5bff
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-04-10 21:16:12 +0200

    making page for each crew member, same code on 4 pages same for html, css, js

--- starter-code/engineer.js
+++ starter-code/engineer.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
 
         if(selectedRate !== null) {
             selectedRate.removeClass('active');
+            $("#bullets:first").removeClass('active')
         }
         $(this).addClass('active');
         selectedRate = $(this);
